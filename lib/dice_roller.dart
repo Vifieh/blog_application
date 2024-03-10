44f8d42608bdb7b1298d7def1f7607e2709319ad
Vifieh <vifiehruth@gmail.com>
2024-03-10 16:55:34 +0100

    first flutter project

---
+++ lib/dice_roller.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:svg_flutter/svg.dart';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 2;
+
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SvgPicture.asset("assets/images/dice-$currentDiceRoll.svg", width: 200),
+        const SizedBox(
+          height: 20,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll Dice'),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/gradient_container.dart
@@ -0,0 +1,26 @@
+import 'package:first_demo/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
